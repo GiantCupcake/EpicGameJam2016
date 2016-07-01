2482e16fecb497cd8bf1048f10f575eeef008365
Guillaume Noguera <Guillaume Noguera>
2016-07-02 00:20:44 +0200

    map + trucs

---
+++ Assets/nrv/Scripts/Generatorrr.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generatorrr : MonoBehaviour {
+
+    int startingX;
+    int startingY;
+    public GameObject CubeRause;
+    public GameObject CubeBleu;
+    public int width;
+    public int height;
+	// Use this for initialization
+	void Start () {
+        Generate();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void Generate()
+    {
+        int colorC = 0;
+        for (int x=0; x<width; x++)
+        {
+            for (int y=0; y<height; y++)
+            {
+                Vector3 Loc = new Vector3(x, 0, y);
+                if (colorC%2 == 0)
+                {
+                    Instantiate(CubeRause, Loc, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(CubeBleu, Loc, Quaternion.identity);
+                }
+
+                colorC++;
+            }
+            colorC++;
+        }
+    }
+}

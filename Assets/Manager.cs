400251a873904b04d2662f4d19b245b5e35a5697
MattDirty <mtorsell@student.42.fr>
2016-07-03 00:53:11 +0200

    UI a bit better

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Manager : MonoBehaviour {
@@ -6,9 +7,18 @@ public class Manager : MonoBehaviour {
     private int layerMaskUnits;
     private int layerMaskTiles;
     public UnitControlScript selected;
+    public Text textUnitName;
+    public Text textUnitHp;
+    public Text textUnitMove;
+    public Text textUnitDamages;
+    public Text textUnitTick;
+    public Text textUnitIntox;
+    public GameObject selectedPanel;
+    public GameObject detonateButton;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         selected = null;
         layerMaskUnits = 1 << 8;
         layerMaskTiles = 1 << 9;
@@ -18,6 +28,24 @@ public class Manager : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    void writeSelected()
+    {
+        detonateButton.SetActive(true);
+        selectedPanel.SetActive(true);
+        textUnitName.text = selected.name;
+        textUnitHp.text = "HP : " + selected.hp.ToString();
+        textUnitMove.text = "Movements left : " + selected.remainingMoves.ToString();
+        textUnitDamages.text = "Damages : " + selected.dmg.ToString();
+        textUnitTick.text = "Turns to detonate : " + selected.bombTick.ToString();
+        textUnitIntox.text = "Intoxication : " + selected.intox.ToString();
+    }
+
+    void clearSelected()
+    {
+        detonateButton.SetActive(false);
+        selectedPanel.SetActive(false);
+    }
     void FixedUpdate()
     {
         RaycastHit hit;
@@ -26,15 +54,24 @@ public class Manager : MonoBehaviour {
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 100.0f, layerMaskUnits))
+            {
                 selected = hit.collider.gameObject.GetComponent<UnitControlScript>();
+                writeSelected();
+            }
             else
+            {
                 selected = null;
+                clearSelected();
+            }
         }
         if (Input.GetMouseButtonDown(1) && selected != null)
         {
             ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 100.0f, layerMaskTiles))
-                    selected.moveTo(hit.transform.position.x, hit.transform.position.z);
+            {
+                selected.moveTo(hit.transform.position.x, hit.transform.position.z);
+                writeSelected();
+            }
         }
     }
 }

bb3f1c6b340ee0d81658463310d295019d9a325e
MattDirty <mtorsell@student.42.fr>
2016-07-02 22:02:51 +0200

    added some kind of pathfinding.

--- Assets/GruntScript.cs
+++ Assets/GruntScript.cs
@@ -4,19 +4,51 @@ using System;
 
 public class GruntScript : UnitControlScript {
 
+    public override void dealDmg()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void getWrecked()
+    {
+        Destroy(this.gameObject);
+    }
+
     public override void moveTo(float x, float y)
     {
-        print("going to " + x + "---" + y);
+        print("My name is : " + this.name + " and I'm going to " + x + "---" + y);
     }
 
     // Use this for initialization
     void Start () {
-	
-	}
+        dmg = 1;
+        hp = 1;
+        maxMove = 3;
+        bombTick = 3;
+        remainingMoves = maxMove;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
+
+    // A FAIRE : VERIFIER QUE LA CASE SOIT LIBRE UNE FOIS QUE NOTRE MAP ARRETE D'ETRE AUTISTE SVP MERCI
+    void pathFinder(int destX, int destY)
+    {
+        while (posX > destX)
+            if (!MoveLeft())
+                break;
+        while (posX < destX)
+            if (!MoveRight())
+                break;
+        while (posY < destY)
+            if (!MoveDown())
+                break;
+        while (posY > destY)
+            if (!MoveUp())
+                break;
+    }
 }
--- Assets/UnitControlScript.cs
+++ Assets/UnitControlScript.cs
@@ -3,9 +3,24 @@ using System.Collections;
 
 public abstract class UnitControlScript : MonoBehaviour {
 
+    public builderScript builder;
+    public int maxMove;
+    public int remainingMoves;
+    public int hp;
+    public int posX;
+    public int posY;
+    public int dmg;
+    public int bombTick;
+    public int intox;
+    public string owner;
+    private int maxX;
+    private int maxY;
+
 	// Use this for initialization
 	void Start () {
-	
+        intox = 1;
+        maxX = builder.MapWidth;
+        maxY = builder.MapHeight;
 	}
 	
 	// Update is called once per frame
@@ -14,4 +29,58 @@ public abstract class UnitControlScript : MonoBehaviour {
 	}
 
     public abstract void moveTo(float x, float y);
+    public abstract void getWrecked();
+    public abstract void dealDmg();
+
+    public bool MoveLeft()
+    {
+        if ((posX > 0) && (remainingMoves > 0))
+        {
+            moveTo(-1, 0);
+            posX--;
+            remainingMoves--;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool MoveRight()
+    {
+        if ((posX < maxX - 1) && (remainingMoves > 0))
+        {
+            moveTo(1, 0);
+            posX++;
+            remainingMoves--;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool MoveUp()
+    {
+        if ((posY > 0) && (remainingMoves > 0))
+        {
+            moveTo(0, -1);
+            posY--;
+            remainingMoves--;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool MoveDown()
+    {
+        if ((posY < maxY - 1) && (remainingMoves > 0))
+        {
+            moveTo(0, 1);
+            posY++;
+            remainingMoves--;
+            return true;
+        }
+        else
+            return false;
+    }
 }

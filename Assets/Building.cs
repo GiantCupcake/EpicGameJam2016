9f003715d9b26e7d2d96d3b8ba0f0ceafa1f5096
DamnStyle <sebastien.martinez@gmail.com>
2016-07-03 16:17:37 +0200

    add bottle

--- Assets/Building.cs
+++ Assets/Building.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Building : MonoBehaviour {
 
-	// Use this for initialization
-	public virtual void Start () {
+    PlayerManagerGreen player1;
+    PlayerManagerRed player2;
+    int LastPlayerActive;
+    int PlayerActive;
 
+
+    // Use this for initialization
+    public virtual void Start () {
+        player1 = FindObjectOfType<PlayerManagerGreen>();
+        player2 = FindObjectOfType<PlayerManagerRed>();
     }
 
     // Update is called once per frame
     public virtual void Update () {
-        //print(gameObject.name + ": x = " + transform.position.x + " - y = " + transform.position.z);
-	}
+
+        if (player1.isActive == true && LastPlayerActive == 2)
+            print("spawn");
+        if (player2.isActive == true && LastPlayerActive == 1)
+            print("spawn");
+    }
 }
